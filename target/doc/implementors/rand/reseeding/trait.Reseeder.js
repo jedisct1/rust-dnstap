19145cf0bbb480f0eb5eba81e887adcec542f0df
Frank Denis <github@pureftpd.org>
2016-11-20 13:45:31 +0100

    Nits

--- target/doc/implementors/rand/reseeding/trait.Reseeder.js
+++ target/doc/implementors/rand/reseeding/trait.Reseeder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["void"] = [];implementors["libc"] = [];implementors["nix"] = [];implementors["mio"] = [];
+implementors["libc"] = [];implementors["void"] = [];implementors["nix"] = [];implementors["mio"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
